745064e72af81b24ce1512b08412e070b3c775b1
kashikichi <60090083+Pikselas@users.noreply.github.com>
2023-07-06 13:46:38 +0530

    Block the thread until the application completes reading

--- SeqDownWebHost/Server.cpp
+++ SeqDownWebHost/Server.cpp
@@ -2,24 +2,63 @@
 #include "Server.h"
 
 
-void OnRequestPath(const char* path, void(*callback)(void* request))
+void OnRequestPath(const char* path, void(*callback)(unsigned int request_id))
 {
 	server.OnPath(path, [&](Request& req, Response& res) {
 		
-		callback(&req);
+		unsigned int locker_id = std::random_device{}();
+		std::cout << "ID_GENERATED: " << locker_id << std::endl;
+		requests.emplace(locker_id, req);
+		callback(locker_id);
 
+		std::cout << "Callback exited" << std::endl;
+
+		requests.at(locker_id).WaitTillFinished();
+
+		std::cout << "Waited till finished" << std::endl;
+
+		res.SendString("Ooh!! Imouto janai desu?");
 	});
 }
 
-int RequestBodyReader(void* request, char* buffer, unsigned int size)
+int RequestBodyReader(unsigned int request_id, char* buffer, unsigned int size)
 {
-	auto req = static_cast<Request*>(request);
-	if(auto read_size = req->ReadBody(std::span<char>(buffer , size)))
-		return *read_size;
-	return -1;
+	std::cout << "Reading body-1" << std::endl;
+	std::cout << "Request id: " << request_id << std::endl;
+	return requests.at(request_id).ReadBody(std::span<char>(buffer, size));
 }
 
 void StartServer()
 {
-	server.Serve();
+	try
+	{
+		server.Serve();
+	}
+	catch (const std::exception& e)
+	{
+		std::cout << "Error: " << e.what() << std::endl;
+	}
+}
+
+RequestReadLocker::RequestReadLocker(Request& request) : request(request)
+{
+	
+}
+
+int RequestReadLocker::ReadBody(std::span<char> buffer)
+{
+	std::cout << "Reading body-2" << buffer.size() << std::endl;
+	if (auto read_size = request.ReadBody(buffer))
+		return *read_size;
+	std::cout << "Finished reading body" << std::endl;
+	finished = true;
+	return -1;
+}
+
+void RequestReadLocker::WaitTillFinished()
+{
+	while (!finished)
+	{
+		std::this_thread::sleep_for(std::chrono::milliseconds(500));
+	};
 }
--- SeqDownWebHost/Server.h
+++ SeqDownWebHost/Server.h
@@ -2,13 +2,32 @@
 #include"pch.h"
 #include<functional>
 #include<memory>
+#include<atomic>
+#include<iostream>
+#include<random>
+#include<map>
 #include"Spike/HttpServer.h"
 
 // HttpServer instance
 HttpServer server(3456);
 
+class RequestReadLocker
+{
+private:
+	std::atomic_bool finished = false;
+private:
+	Request& request;
+public:
+	RequestReadLocker(Request& request);
+public:
+	int ReadBody(std::span<char> buffer);
+	void WaitTillFinished();
+};
+
+std::map<unsigned int, RequestReadLocker> requests;
+
 extern "C" {
-	__declspec(dllexport) void OnRequestPath(const char* path , void(*callback)( void * request ));
-	__declspec(dllexport) int RequestBodyReader(void * request , char* buffer , unsigned int size);
+	__declspec(dllexport) void OnRequestPath(const char* path , void(*callback)( unsigned int request_id ));
+	__declspec(dllexport) int RequestBodyReader(unsigned int request_id , char* buffer , unsigned int size);
 	__declspec(dllexport) void StartServer();
 }
